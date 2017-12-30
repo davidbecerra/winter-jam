25bee0b3366a2f6db430d7e897b1abe74837517f
elisabeth <wak@wakamac.local>
2017-12-30 01:06:46 -0500

    Added Title Screen with idling dog and penguino

---
+++ Assets/TitleScreenIdlingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenIdlingController : MonoBehaviour {
+
+	// create animator variable
+	Animator PenguinAnimator;
+
+
+	// Use this for initialization
+	void Start () {
+		// import the animator component
+		PenguinAnimator = GetComponent<Animator>();
+
+		PenguinAnimator.SetTrigger("Idling");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
