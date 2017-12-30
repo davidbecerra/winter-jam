91709815ddc1cff90509130843c45c3da0a95f1b
David <davidbecerra@users.noreply.github.com>
2017-12-29 22:27:09 -0800

    Add level transition

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+            EditorSceneManager.LoadScene("Main");
+	}
+}
