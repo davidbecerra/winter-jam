3b65fa532662e55c76c179eb6ece8ec88c520c14
iconshocking <shockconrad@gmail.com>
2017-12-29 20:14:53 -0800

    dash change to single press

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,9 +2,9 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-
     public enum PlayerNumber { One, Two }
     public PlayerNumber playerNum = PlayerNumber.One;
+    private int DASH_FRAMES = 5;
 
     private Dictionary<string, KeyCode> playerOneControls = new Dictionary<string, KeyCode> {
         { "Up", KeyCode.W },
@@ -22,9 +22,11 @@ public class PlayerMovement : MonoBehaviour {
         {"Dash", KeyCode.RightShift }
     };
 
-    private float movementSpeed = 0.5f;
-    private float dashSpeed = 1.5f;
+    private float movementSpeed = 0.2f;
+    private float dashSpeed = 1.2f;
     private Dictionary<string, KeyCode> controls;
+    private bool isDashing = false;
+    private int dashFramesRemaining = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -33,7 +35,12 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float speed = Input.GetKey(controls["Dash"]) ? dashSpeed : movementSpeed;
+        if (Input.GetKeyDown(controls["Dash"]) && !isDashing)
+        {
+            isDashing = true;
+            dashFramesRemaining = DASH_FRAMES;
+        }
+        float speed = isDashing ? dashSpeed : movementSpeed;
         if (Input.GetKey(controls["Up"]))
         {
             transform.Translate(Vector3.up * speed);
@@ -51,5 +58,14 @@ public class PlayerMovement : MonoBehaviour {
             transform.Translate(Vector3.left * speed);
         }
 
+        if (dashFramesRemaining > 0)
+        {
+            --dashFramesRemaining;
+        }
+
+        if (dashFramesRemaining == 0)
+        {
+            isDashing = false;
+        }
     }
 }
