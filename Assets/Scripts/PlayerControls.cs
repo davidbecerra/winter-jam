dc14b0bca6ba624a588f6f0fe3d0fff4ffdb08f2
David <davidbecerra@users.noreply.github.com>
2018-01-09 14:57:31 -0800

    Add player control scriptable object

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerControls : ScriptableObject {
+    public KeyCode Up;
+    public KeyCode Down;
+    public KeyCode Left;
+    public KeyCode Right;
+    public KeyCode Dash;
+    public KeyCode Throw;
+}
